043ff798890e3060bcdf4e9b90ed7a8a23be5ec7
sergzaligin359 <webdevjsphp@gmail.com>
2021-01-10 16:29:06 +0300

    fix

--- src/pages/todos/components/TodoList.tsx
+++ src/pages/todos/components/TodoList.tsx
@@ -1,30 +1,33 @@
+import { useContext } from "react";
+import { ContextTodo } from "../../../state/todo/types/context";
 
 export const TodoList: React.FC = () => {
+
+    const {state, } = useContext(ContextTodo);
+
     return (
         <ul>
             {
-                // todos.map(({ id, title, completed }) => {
-                //     const classes = ['todo']
-                //     if(completed){
-                //         classes.push('completed')
-                //     }
-                //     return (
-                //             <li 
-                //                 onChange={ () => toggleTodo(id) }
-                //                 className={ classes.join(' ') } 
-                //                 key={id}
-                //             >
-                //                 <label>
-                //                     <input type="checkbox" defaultChecked={completed} />
-                //                     <span>{ title }</span>
-                //                     <i 
-                //                         onClick={ () => removeTodo(id) }
-                //                         className="material-icons red-text"
-                //                     >delete</i>
-                //                 </label>
-                //             </li>
-                //         )
-                //     })
+                state?.todos.map(({ title, completed }) => {
+                    const classes = ['todo']
+                    if(completed){
+                        classes.push('completed')
+                    }
+                    return (
+                            <li 
+                                className={ classes.join(' ') } 
+                                key={title}
+                            >
+                                <label>
+                                    <input type="checkbox" defaultChecked={completed} />
+                                    <span>{ title }</span>
+                                    <i 
+                                        className="material-icons red-text"
+                                    >delete</i>
+                                </label>
+                            </li>
+                        )
+                    })
             }
 
         </ul>
