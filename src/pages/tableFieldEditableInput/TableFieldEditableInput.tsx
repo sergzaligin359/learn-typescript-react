285610f23baea0daa2d48920a4b7b34dfae5b4e5
Залыгин Сергей <zalyginsv@it2g.ru>
2021-01-18 14:05:13 +0300

    fix

--- src/pages/tableFieldEditableInput/TableFieldEditableInput.tsx
+++ src/pages/tableFieldEditableInput/TableFieldEditableInput.tsx
@@ -1,18 +1,21 @@
 
 import React, { useContext, useState, useEffect, useRef, FC } from 'react';
-import { Table, Input, Button, Popconfirm, Form, Space, Tag } from 'antd';
+import { Table, Input, Button, Popconfirm, Form, Space, Tag, DatePicker } from 'antd';
 import { FormInstance } from 'antd/lib/form';
+import moment from 'moment';
 
 const EditableContext = React.createContext<FormInstance<any> | null>(null);
 
 interface EditableRowProps {
   index: number;
 }
-
+const worker = {
+  date: moment('2020-06-09T12:40:14+0000')
+};
 const EditableRow: React.FC<EditableRowProps> = ({ index, ...props }) => {
   const [form] = Form.useForm();
   return (
-    <Form form={form} component={false}>
+    <Form form={form} component={false} initialValues={worker}>
       <EditableContext.Provider value={form}>
         <tr {...props} />
       </EditableContext.Provider>
@@ -38,8 +41,11 @@ const EditableCell: React.FC<EditableCellProps> = ({
   handleSave,
   ...restProps
 }) => {
+  const [type, setType] = useState('input');
+  const [date, setDate] = useState<any>(moment());
   const [editing, setEditing] = useState(false);
-  const inputRef = useRef<Input>(null);
+  const inputRef = useRef<any>(null);
+
   const form = useContext(EditableContext)!;
 
   useEffect(() => {
@@ -47,18 +53,45 @@ const EditableCell: React.FC<EditableCellProps> = ({
       inputRef.current!.focus();
     }
   }, [editing]);
-
+  const onChangeDate = (e: any) => {
+    //console.log('DATE event change', e.format('YYYY/MM/DD'));
+    setDate(e.format('YYYY/MM/DD'))
+  }
   const toggleEdit = () => {
+    // console.log('form get value', form.getFieldsValue())
+    if(dataIndex == 'date'){
+      setType('date');
+      // console.log('dataIndex', dataIndex)
+      //console.log('record[dataIndex] date', record)
+      // form.setFieldsValue({ date: "2000-01-15T13:24:46.389" });
+      form.setFieldsValue({ [dataIndex]: date });
+    }else{
+      setType('input');
+      form.setFieldsValue({ [dataIndex]: record[dataIndex] });
+    }
     setEditing(!editing);
-    form.setFieldsValue({ [dataIndex]: record[dataIndex] });
+    //console.log('TYPE INPUT: dataIndex', type, dataIndex);
   };
 
   const save = async () => {
     try {
-      const values = await form.validateFields();
+      // const values = await form.validateFields();
+      // console.log('VALUES in save', values)
+      // toggleEdit();
+      // handleSave({ ...record, ...values });
 
-      toggleEdit();
-      handleSave({ ...record, ...values });
+      // Work solution
+      if(dataIndex == 'date'){
+        toggleEdit();
+        handleSave({ ...record, date });
+      }else{
+        const values = await form.validateFields();
+        //console.log('VALUES in save', values)
+        toggleEdit();
+        handleSave({ ...record, ...values });
+      }
+      // end
+
     } catch (errInfo) {
       console.log('Save failed:', errInfo);
     }
@@ -67,24 +100,36 @@ const EditableCell: React.FC<EditableCellProps> = ({
   let childNode = children;
 
   if (editable) {
-    childNode = editing ? (
-      <Form.Item
-        style={{ margin: 0 }}
-        name={dataIndex}
-        rules={[
-          {
-            required: true,
-            message: `${title} is required.`,
-          },
-        ]}
-      >
-        <Input ref={inputRef} onPressEnter={save} onBlur={save} />
-      </Form.Item>
-    ) : (
-      <div className="editable-cell-value-wrap" style={{ paddingRight: 24 }} onClick={toggleEdit}>
-        {children}
-      </div>
-    );
+    if(type !== 'date'){
+      childNode = editing ? (
+        <Form.Item
+          style={{ margin: 0 }}
+          name={dataIndex}
+          rules={[
+            {
+              required: true,
+              message: `${title} is required.`,
+            },
+          ]}
+        >
+          <Input ref={inputRef} onPressEnter={save} onBlur={save} />
+        </Form.Item>
+      ) : (
+        <div className="editable-cell-value-wrap" style={{ paddingRight: 24 }} onClick={toggleEdit}>
+          {children}
+        </div>
+      );
+    }else{
+      const dateFormat = "YYYY/MM/DD";
+      childNode = editing ? (
+           <DatePicker ref={inputRef}  onBlur={save} name="date" onChange={ (e) => onChangeDate(e) } />
+      ) : (
+        <div className="editable-cell-value-wrap" style={{ paddingRight: 24 }} onClick={toggleEdit}>
+          {children}
+        </div>
+      );
+    }
+
   }
 
   return <td {...restProps}>{childNode}</td>;
@@ -100,67 +145,11 @@ interface EditableTableState {
 type ColumnTypes = Exclude<EditableTableProps['columns'], undefined>;
 
 
-// dataIndex	Display field of the data record, support nest path by string array	string | string[]
-// key	Unique key of this selection	string
-// const columns = [
-//   {
-//     title: 'Имя',
-//     dataIndex: 'name',
-//     key: 'name',
-//   },
-//   {
-//     title: 'Возраст',
-//     dataIndex: 'age',
-//     key: 'age',
-//   },
-//   {
-//     title: 'Адрес',
-//     dataIndex: 'address',
-//     key: 'address',
-//   },
-//   {
-//     title: 'Тэги',
-//     key: 'tags',
-//     dataIndex: 'tags',
-//     render: (tags: string[]) => (
-//       <>
-//         {tags.map((tag: string) => {
-//           let color = tag.length > 5 ? 'geekblue' : 'green';
-//           if (tag === 'loser') {
-//             color = 'volcano';
-//           }
-//           return (
-//             <Tag color={color} key={tag}>
-//               {tag.toUpperCase()}
-//             </Tag>
-//           );
-//         })}
-//       </>
-//     ),
-//   },
-//   {
-//     title: null,
-//     key: 'action',
-//     render: (record: IUsers) => {
-//       // console.log('record', record);
-//       return (
-//           <Space size="middle">
-//             <a>Invite {record.name}</a>
-//             <a>Delete</a>
-//           </Space>
-//       )
-//     }
-//   },
-// ];
-
-// interface ITags{
-//   tags: string[] | undefined
-// }
-
 interface IUsers{
   id: string
   name: string
   age: number
+  date: string | null
   address: string
   tags: string[]
 }
@@ -178,6 +167,16 @@ export const TableFieldEditableInput: FC = <EditableTableProps, EditableTableSta
         title: 'Возраст',
         dataIndex: 'age',
         key: 'age',
+        editable: true,
+      },
+      {
+        title: 'Дата',
+        dataIndex: 'date',
+        key: 'date',
+        editable: true,
+        render: (date: any): any =>{
+          return date ? moment(date).format('YYYY/MM/DD') : ' '
+        }
       },
       {
         title: 'Адрес',
@@ -225,6 +224,7 @@ export const TableFieldEditableInput: FC = <EditableTableProps, EditableTableSta
         id: '1',
         name: 'John Brown',
         age: 32,
+        date: "2021-01-09T15:30:11.426",
         address: 'New York No. 1 Lake Park',
         tags: ['nice', 'developer'],
       },
@@ -232,6 +232,7 @@ export const TableFieldEditableInput: FC = <EditableTableProps, EditableTableSta
         id: '2',
         name: 'Jim Green',
         age: 42,
+        date: "2021-01-09T15:30:11.426",
         address: 'London No. 1 Lake Park',
         tags: ['loser'],
       },
@@ -239,6 +240,7 @@ export const TableFieldEditableInput: FC = <EditableTableProps, EditableTableSta
         id: '3',
         name: 'Joe Black',
         age: 32,
+        date: null,
         address: 'Sidney No. 1 Lake Park',
         tags: ['cool', 'teacher'],
       },
@@ -246,7 +248,7 @@ export const TableFieldEditableInput: FC = <EditableTableProps, EditableTableSta
 
     const [users, setUsers] = useState<IUsers[]>(data)
 
-    console.log('USERS data: ', users)
+    // console.log('USERS data: ', users)
 
     const handleSave = (row: IUsers) => {
       const newData = [...users];
@@ -265,13 +267,14 @@ export const TableFieldEditableInput: FC = <EditableTableProps, EditableTableSta
       setUsers(users.filter(item => item.id != id));
 
     };
-console.log('After del users', users)
+    console.log('Before update users', users)
     const handleAdd = () => {
 
       const newData: IUsers = {
         id: Date.now().toString(),
         name: `Edward King`,
         age: 32,
+        date: null,
         address: `London, Park Lane no.`,
         tags: ['newTag']
       };
@@ -315,3 +318,43 @@ console.log('After del users', users)
         </>
     )
 }
+
+
+const stagesForCard: any = [
+    {
+        name: "Приглашение",
+        status: null,
+        date: null,
+        id: 1
+    },
+    {
+        name: "Первичное собеседование",
+        status: null,
+        date: null,
+        id: 2
+    },
+    {
+        name: "Собеседование с руководителем",
+        status: null,
+        date: null,
+        id: 3
+    },
+    {
+        name: "wwwwww",
+        status: null,
+        date: null,
+        id: 6
+    },
+    {
+        name: "dddd222",
+        status: null,
+        date: "2021-01-09T15:30:11.426",
+        id: 7
+    },
+    {
+        name: "HR интервью",
+        status: null,
+        date: "2021-01-15T13:24:46.389",
+        id: 5
+    }
+]
